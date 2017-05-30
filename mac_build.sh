267fef935e7122495f42f54affaec326161fabe4
Steven C. Howell <steven.howell@nist.gov>
2017-05-30 02:19:01 -0400

    reverted step to remove zip

--- mac_build.sh
+++ mac_build.sh
@@ -4,3 +4,7 @@ pyinstaller --noconfirm --clean --onefile \
             --icon=ttr_icon.png \
             -p /Users/schowell/data/myPrograms/anaconda3/envs/ttr35/lib/python3.5/site-packages/appJar/ \
             ./gui_ttr.py
+
+# zip the result to keep it executable
+cd mac_app
+zip ttr.zip ttr
