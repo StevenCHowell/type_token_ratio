044006f657a5ab8c594c194d1ef2a8accee22a6c
Steven C. Howell <steven.c.howell@gmail.com>
2017-05-30 01:52:07 -0400

    remove icon from build script

--- win_build.sh
+++ win_build.sh
@@ -1,10 +1,9 @@
 pyinstaller --noconfirm --clean --onefile \
             --distpath win_app \
             -n ttr \
-            --icon=ttr_icon.png \
             -p /c/Users/schowell/Anaconda3/envs/ttr35/lib/python3.5/site-packages/appJar/ \
             ./gui_ttr.py
 
 # zip up the result
-cd win_app/
-zip ttr.zip ttr
+# cd win_app/
+# zip ttr.zip ttr
